886ef3bf83907e852fd7fc974a81fc44617a7370
Suhail Paliath <suhail.paliath@live.com>
2014-06-22 12:24:24 +0300

    commit 1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Create a cacheble matrix type. Has the ability to cache the
+## result and look it up in the future to improve performance.
 
-## Write a short comment describing this function
+## Create matrix type.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  
+  ## Set matrix
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  
+  ## Get matrix
+  get <- function() x
+  
+  ## Set cached matrix
+  setCache <- function(matinv) inv <<- matinv
+  
+  ## Get cached matrix
+  getCache <- function() inv
+  
+  ## Return cachedmatrix type
+  list(set = set, get = get, setCache = setCache, getCache = getCache)
 }
 
 
-## Write a short comment describing this function
+## Return inverse of cachedmatrix x. Looks up cache if available. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getCache()
+  
+  ## Check if matrix has already been cached
+  if(!is.null(inv)) {
+    message("Getting cached matrix.")
+    return(inv)
+  }
+  
+  ## Get matrix
+  data <- x$get()
+  
+  ## Solve matrix
+  inv <- solve(data, ...)
+  
+  ## Set cache
+  x$setCache(inv)
+  
+  ## Return inversed matrix
+  inv
 }
